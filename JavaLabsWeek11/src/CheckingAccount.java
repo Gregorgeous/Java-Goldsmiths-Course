32cb111159bede54d9d3812a67e07306ca29c750
Grzegorz Rybak Gryba002 <g.r.fisher.pl@gmail.com>
2018-01-22 13:41:47 +0000

    updated the javaLabsWeek11 and have all labs again to merge in master

--- JavaLabsWeek11/src/CheckingAccount.java
+++ JavaLabsWeek11/src/CheckingAccount.java
@@ -1,9 +1,11 @@
 public class CheckingAccount extends Account{
     private int numberOfChecksUsed; // stores the number of checks used every month. Starts at zero.
-    private double ATMTransactionFee = 1.00;
+    private double ATMTransactionFee = 1.00; // (Exercise requirement) transaction fee for a normal (atm) withdraw and deposit methods.
+//    Constructor calling its super, abstract class
     public CheckingAccount(String id, double initialBalance){
         super(id, initialBalance);
     }
+//    Same as in Saving account, check if operation possible, and do it returning true, otherwise log an error and return false.
     public boolean withdraw(double amount){
         if (balance - amount - ATMTransactionFee >= 0 ){
             balance = balance - amount - ATMTransactionFee;
@@ -13,16 +15,20 @@ public class CheckingAccount extends Account{
             return false;
         }
     }
+//    Deposit money minus a transaction fee applying for that in this type of account.
     public void deposit(double amount){
         balance = balance + amount - ATMTransactionFee;
     }
+//    method can be called e.g. by bank class. Resets the num of checks used for this (or next) month.
     public void resetChecksUsed(){
         numberOfChecksUsed = 0;
         System.out.println("Checking account: " + this.id + " -> reset num of checks used");
     }
+//    Simple getter
     public int getChecksUsed(){
         return numberOfChecksUsed;
     }
+//    Withdraw, but usin a check. You have 3 checks usages for free (first condition), then widthraw using checks costs £2. Again, in the inner condition of both upper if's we check if operation is not "illegal".
     public boolean withdrawUsingCheck(double amount){
         if (numberOfChecksUsed <= 3){
             if (balance - amount >= -10){
--- JavaLabsWeek11/src/SavingsAccount.java
+++ JavaLabsWeek11/src/SavingsAccount.java
@@ -1,29 +1,36 @@
 public class SavingsAccount extends Account {
+//    Constructor initialising all class variables.
     public SavingsAccount(String id, double initialDeposit){
         super(id, initialDeposit);
         if (initialDeposit >= 1000){
             balance += 10;
         }
     }
+//    Method allows to withdraw money but with a transaction fee applied to this type of bank account
     public boolean withdraw(double amount){
       double transactionFee = 3;
+//      We check if the final amount of money after withdrawing and applying the fee is no less than £10 (exercise's requirement)
       if (balance - amount - transactionFee >= 10){
+//          If so, update new balance of the account and return true
           balance = balance - amount - transactionFee;
           return true;
       }
+//      Otherwise, print a message and return false.
       else {
           System.out.println("Operation not allowed: insufficient funds");
           return false;
       }
     }
+//    Self-explanatory
     public void deposit(double amount) {
         balance += amount;
     }
+//  Here we take a value of the balance, calculates new balance with an interest rate provided and updates the account balance with a new value. Returns the updated account balance value.
     public double addInterest(double rate){
         double balance = getBalance();
-        double newBalance = balance + balance*rate;
-        this.balance= newBalance;
-        System.out.println("I added interest to savings account : " + this.id + ". Was: " + balance + ", now is: " + newBalance);
-        return newBalance;
+        this.balance= balance + balance*rate;
+//        This message below is only for testing purposes when you call the method in main so that you see something is actually happening.
+        System.out.println("I added interest to savings account : " + this.id + ". Was: " + balance + ", now is: " + this.balance);
+        return this.balance;
     }
 }
