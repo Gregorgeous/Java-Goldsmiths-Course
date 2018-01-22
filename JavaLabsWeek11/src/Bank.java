fc3e766c719732c0de6a191e2e8646f5be5e61b5
gr.eg <g.r.fisher.pl@gmail.com>
2018-01-22 11:29:00 +0000

    added second part of comments

--- JavaLabsWeek11/src/Bank.java
+++ JavaLabsWeek11/src/Bank.java
@@ -1,17 +1,24 @@
 import java.util.ArrayList;
 
+// A class managing the account classes.
 public class Bank {
+//    Initialising arraylist of accounts.
     private ArrayList<Account> accounts;
+//    Creating savingsInterestRate class variable (value is a percentage)
     private double savingsInterestRate = 0.1;
+//    Constructor function where we initialise accounts arraylist.
     public Bank(){
         accounts = new ArrayList<Account>();
     }
+//    return num of accounts
     public int numberOfAccounts(){
         return accounts.size();
     }
+//    add account to arraylist.
     public void addAccount(Account a){
         accounts.add(a);
     }
+//    Traverse through arraylist of accounts and return the first account matching accountID you have provided. If nothing found, return null.
     public Account getAccount(String accountID){
         for (Account acc : accounts) {
             if (acc.getID().equals(accountID)){
@@ -19,14 +26,10 @@ public class Bank {
             }
         }
         return null;
-//        int accountIndex = accounts.indexOf(Account.getID(accountID));
-//        if (accountIndex == -1){
-//            return null;
-//        }else {
-//            return accounts.get(accountIndex);
-//        }
-
     }
+//    Deposit money from bank's perspective - provide accountID of an account you want to deposit money.
+//    Then find this account calling "getAccount" method above.
+//    If found , deposit money, otherwise return false.
     public boolean deposit(String accountID, double amount){
         Account targetAccount = this.getAccount(accountID);
         if (targetAccount == null){
@@ -36,6 +39,7 @@ public class Bank {
             return true;
         }
     }
+//    Same as deposit but here in successful case we return a result of targetAccount.withdraw method (method's true or false results is dependent on inner conditions there)
     public boolean withdraw(String accountID, double amount){
         Account targetAccount = this.getAccount(accountID);
         if (targetAccount == null){
@@ -44,15 +48,22 @@ public class Bank {
             return targetAccount.withdraw(amount);
         }
     }
+//  same as withdraw but with an extra condition if withdraw was successful first.
     public boolean transfer(String fromAccountID, String toAccountID, double amount){
         Account targetAccount1 = this.getAccount(fromAccountID);
         Account targetAccount2 = this.getAccount(toAccountID);
         if (targetAccount1 == null || targetAccount2 == null){
             return false;
         }else {
-            targetAccount1.withdraw(amount);
-            targetAccount2.deposit(amount);
-            return true;
+            boolean withdrawSuccess = targetAccount1.withdraw(amount);
+            if (withdrawSuccess){
+                targetAccount2.deposit(amount);
+                return true;
+            }
+            else {
+                return false;
+            }
+
         }
     }
     public void addInterest() {
